6e2ebbc8bace37042ad7618f48e69a45041f7e42
MIhail <mimatyatsko@gmail.com>
2022-08-08 11:18:46 +0300

    - final example app version (Swift 5.6) - changed credentials placeholders

--- CombineExample/Workers/NetworkWorker/NetworkWorker.swift
+++ CombineExample/Workers/NetworkWorker/NetworkWorker.swift
@@ -20,8 +20,8 @@ final class NetworkWorker {
     func tryToSignIn(with login: String, password: String) -> Future<SignInResponse, SignInErrors> {
         return Future { promise in
             DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                let receivedLogin = "Misha1"
-                let receivedPassword = "1234WXS"
+                let receivedLogin = "Login"
+                let receivedPassword = "Password"
                 if login == receivedLogin && password == receivedPassword {
                     promise(.success(.init(userLogin: receivedLogin, isSignedIn: true)))
                 } else {
